f2732a2b56452ee6d67ebefeaef33aa428485d2c
waldoeGeek <thomas.reynolds@student.sl.on.ca>
2021-02-17 00:35:43 -0500

    added keyword for search type

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # **Reddit Search Extension**  
-[ ![shiled](https://img.shields.io/badge/Ulauncher-Extension-informational?style=flat-square)](https://ulauncher.io)  [ ![license](https://img.shields.io/github/license/waldoeGeek/ulauncher-reddit-search?style=flat-square)](https://github.com/waldoeGeek/ulauncher-reddit-search/blob/master/LICENSE)  ![commit](https://img.shields.io/github/last-commit/waldoeGeek/ulauncher-reddit-search?style=flat-square)  
+[ ![shiled](https://img.shields.io/badge/Ulauncher-Extension-informational?style=flat-square)](https://ulauncher.io) [ ![license](https://img.shields.io/github/license/waldoeGeek/ulauncher-reddit-search?style=flat-square)](https://github.com/waldoeGeek/ulauncher-reddit-search/blob/master/LICENSE)  ![commit](https://img.shields.io/github/last-commit/waldoeGeek/ulauncher-reddit-search?style=flat-square)  
 ![version](https://img.shields.io/badge/Version-V1.0-important?style=flat-square) ![size](https://img.shields.io/github/languages/code-size/waldoeGeek/ulauncher-reddit-search?style=flat-square)
 
 
@@ -7,7 +7,7 @@
 
 ### Disclaimer  
 I am a beginner with python. This project was intended for myself.
-### Search Reddit For:  
+### **Search Reddit For:**  
 
 -Users  
 -Subreddits  
@@ -27,8 +27,18 @@ Open Ulauncher go to preferences -> extensions -> add extension and paste:
 ```
 https://github.com/waldoeGeek/ulauncher-reddit-search
 ```  
-## Useage   
+## **Useage**   
 
 To search, type the keyword. Default is `rt` and type your search.  
-To change search criteria:  
-Go to Preferences window -> extensions -> reddit extension and select appropriate result type. 
+Change Result Count: Preference window -> extensions -> reddit-search -> Macimum results dropdown  
+To change search criteria...  
+
+## **Keywords**
+Default keywords are  
+`rt` : Search all of Reddit  
+`rtu` : Search only Users  
+`rtr` : Search only Sub-Reddits  
+
+## **Actions**  
+
+-Press `enter` to open result in browser
--- main.py
+++ main.py
@@ -41,21 +41,38 @@ class KeywordQueryEventListener(EventListener):
                     ExtensionResultItem(
                         icon=ICON_FILE,
                         name='Keep Typing to Search on Reddit....',
-                        on_enter=HideWindowAction(),
+                        on_enter=DoNothingAction(),
                         highlightable=False
                     )
                 ])
 
-            # Check result type options
+        # Check result type options
             try:
-                if extension.preferences["result_types"] == 'all':
-                    data = Searches.get_results(REDDIT_URL, query)
-                elif extension.preferences['result_types'] == 'sub':
-                    data = Searches.get_subs(REDDIT_URL, query)
-                elif extension.preferences['result_types'] == 'user':
+                # if extension.preferences["result_types"] == 'all':
+                #     data = Searches.get_results(REDDIT_URL, query)
+                # elif extension.preferences['result_types'] == 'sub':
+                #     data = Searches.get_subs(REDDIT_URL, query)
+                # elif extension.preferences['result_types'] == 'user':
+                #     data = Searches.get_users(REDDIT_URL, query)
+                keyword = event.get_keyword()
+                for kw_id, kw in list(extension.preferences.items()):
+                    if kw == keyword:
+                        keyword_id = kw_id
+
+                data = Searches.get_results(REDDIT_URL, query)
+                if keyword_id == "rtu_kw":
                     data = Searches.get_users(REDDIT_URL, query)
+                elif keyword_id == "rtr_kw":
+                    data = Searches.get_subs(REDDIT_URL, query)
+
             except Exception as e:
-                raise
+                items.append(ExtensionResultItem(
+                    icon='images/icon.png',
+                    name=f'ERROR {e}',
+                    description='failed at keyword',
+                    on_enter=HideWindowAction()))
+
+                return RenderResultListAction(items)
 
 
             max_results = int(extension.preferences['max_results'])
@@ -66,7 +83,7 @@ class KeywordQueryEventListener(EventListener):
                 items.append(ExtensionResultItem(
                     icon=ICON_FILE,
                     name=child['data']['title'],
-                    description=f"/r/{child['data']['subreddit']}",
+                    description=f"{child['data']['subreddit']}",
                     on_enter=OpenUrlAction(child_url)
                 ))
 
